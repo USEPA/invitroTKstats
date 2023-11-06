611377916de3b3de431b9b2b5fd8685037ca1915
Zhao <Zhao.Zhihui@epa.gov>
2023-11-06 10:23:05 -0500

    re-ordered argument documentations to match the order they are defined in the function

--- invitroTKstats/R/format_clint.R
+++ invitroTKstats/R/format_clint.R
@@ -33,11 +33,11 @@
 #' spectrometry (MS) sample name used by the laboratory. (Defaults to
 #' "Lab.Sample.Name".)
 #' 
-#' @param date.col (Character) Column name of \code{data.in} containing the laboratory measurement
-#' date. (Defaults to "Date".)
-#' 
 #param date A single value to be assigned for all samples as 
 #the laboratory measurement date (Defaults to NULL).
+#'
+#' @param date.col (Character) Column name of \code{data.in} containing the laboratory measurement
+#' date. (Defaults to "Date".)
 #' 
 #' @param compound.col (Character) Column name of \code{data.in} containing the test compound.
 #' (Defaults to "Compound.Name".)
@@ -51,27 +51,21 @@
 #' @param type.col (Character) Column name of \code{data.in} containing the sample type (see table
 #' under Details). (Defaults to "Sample.Type".)
 #' 
+#' @param density (Numeric) The density (units of
+#' millions of hepatocytes per mL) hepatocytes in the in vitro incubation. 
+#' (Defaults to \code{NULL}.) (Note: Single entry only, 
+#' use only if all tested compounds have the same density.)
+#' 
 #' @param density.col (Character) Column name containing \code{density} 
 #' information. (Defaults to "Hep.Density".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{density}.)
 #' 
-#' @param density (Numeric) The density (units of
-#' millions of hepatocytes per mL) hepatocytes in the in vitro incubation. 
-#' (Defaults to \code{NULL}.) (Note: Single entry only, 
-#' use only if all tested compounds have the same density.)
-#'
-#param compound.conc.col Which column of \code{data.in} indicates the intended concentration 
-#of the test chemical for calibration curves (Defaults to "Nominal.Conc")
-#'
 #param compound.conc A single value to be assigned for all samples as 
 #the intended concentration of the test chemical for calibration curves 
 #(Defaults to NULL).
-#'
-#' @param cal.col (Character) Column name containing \code{cal} 
-#' information. (Defaults to "Cal".) (Note: \code{data.in} does not
-#' necessarily have this field. If this field is missing, it can be auto-filled with the value 
-#' specified in \code{cal}.)
+#param compound.conc.col Which column of \code{data.in} indicates the intended concentration 
+#of the test chemical for calibration curves (Defaults to "Nominal.Conc")
 #' 
 #' @param cal (Character) MS calibration the samples were based on. Typically, this uses 
 #' indices or dates to represent if the analyses were done on different machines on 
@@ -79,113 +73,118 @@
 #' (Note: Single entry only, 
 #' use only if all data were collected based on the same calibration.)
 #' 
+#' @param cal.col (Character) Column name containing \code{cal} 
+#' information. (Defaults to "Cal".) (Note: \code{data.in} does not
+#' necessarily have this field. If this field is missing, it can be auto-filled with the value 
+#' specified in \code{cal}.)
+#' 
+#' @param dilution (Numeric) Number of times the sample was diluted before MS 
+#' analysis. (Defaults to \code{NULL}.) (Note: Single entry only, use only if all 
+#' samples underwent the same number of dilutions.)
+#' 
 #' @param dilution.col (Character) Column name containing \code{dilution} 
 #' information. (Defaults to "Dilution.Factor".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{dilution}.)
 #' 
-#' @param dilution (Numeric) Number of times the sample was diluted before MS 
-#' analysis. (Defaults to \code{NULL}.) (Note: Single entry only, use only if all 
-#' samples underwent the same number of dilutions.)
+#' @param time (Numeric) The intended time of the measurement (in minutes) since the test 
+#' chemicals was introduced into the hepatocyte incubation. (Defaults to 2.)
 #' 
 #' @param time.col (Character) Column name containing \code{time} 
 #' information. (Defaults to "Time".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{time}.)
 #' 
-#' @param time (Numeric) The intended time of the measurement (in minutes) since the test 
-#' chemicals was introduced into the hepatocyte incubation. (Defaults to 2.) 
-#' 
 #' @param istd.col (Character) Column name of \code{data.in} containing the
 #' MS peak area for the internal standard. (Defaults to "ISTD.Area".)
-#'
+#' 
+#' @param istd.name (Character) The identity of the internal standard. (Defaults to \code{NULL}.) 
+#' (Note: Single entry only, use only if all tested compounds use the same internal standard.) 
+#' 
 #' @param istd.name.col (Character) Column name containing \code{istd.name} information. 
 #' (Defaults to "ISTD.Name".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{istd.name}.)
 #' 
-#' @param istd.name (Character) The identity of the internal standard. (Defaults to \code{NULL}.) 
-#' (Note: Single entry only, use only if all tested compounds use the same internal standard.) 
+#' @param istd.conc (Numeric) The concentration for the internal standard. (Defaults to \code{NULL}.) 
+#' (Note: Single entry only, use only if all tested compounds have the same 
+#' internal standard concentration.) 
 #'
 #' @param istd.conc.col (Character) Column name containing \code{istd.conc} information. 
 #' (Defaults to "ISTD.Conc".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{istd.conc}.)
 #' 
-#' @param istd.conc (Numeric) The concentration for the internal standard. (Defaults to \code{NULL}.) 
-#' (Note: Single entry only, use only if all tested compounds have the same 
-#' internal standard concentration.) 
+#' @param std.conc (Numeric) The standard test chemical concentration for 
+#' the intrinsic clearance assay. (Defaults to \code{NULL}.) (Note: Single entry only, 
+#' use only if the same standard concentration was used for all tested compounds.)
 #' 
 #' @param std.conc.col (Character) Column name containing \code{std.conc} 
 #' information. (Defaults to "Standard.Conc".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{std.conc}.)
 #' 
-#' @param std.conc (Numeric) The standard test chemical concentration for 
+#' @param clint.assay.conc (Numeric) The initial test chemical concentration for 
 #' the intrinsic clearance assay. (Defaults to \code{NULL}.) (Note: Single entry only, 
-#' use only if the same standard concentration was used for all tested compounds.)
+#' use only if the same initial concentration was used for all tested compounds.)
 #' 
 #' @param clint.assay.conc.col (Character) Column name containing \code{clint.assay.conc} 
 #' information. (Defaults to "Clint.Assay.Conc".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{clint.assay.conc}.)
 #' 
-#' @param clint.assay.conc (Numeric) The initial test chemical concentration for 
-#' the intrinsic clearance assay. (Defaults to \code{NULL}.) (Note: Single entry only, 
-#' use only if the same initial concentration was used for all tested compounds.)
-#' 
 #' @param area.col (Character) Column name of \code{data.in} containing the target analyte (that
 #' is, the test compound) MS peak area. (Defaults to "Area".)
+#'
+#' @param analysis.method (Character) The analytical chemistry analysis method, 
+#' typically "LCMS" or "GCMS", liquid chromatography or gas chromatography–mass spectrometry, respectively. 
+#' (Defaults to \code{NULL}.) (Note: Single entry only, 
+#' use only if the same method was used for all tested compounds.)
 #'
 #' @param analysis.method.col (Character) Column name containing \code{analysis.method} 
 #' information. (Defaults to "Analysis.Method".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{analysis.method}.)
 #' 
-#' @param analysis.method (Character) The analytical chemistry analysis method, 
-#' typically "LCMS" or "GCMS", liquid chromatography or gas chromatography–mass spectrometry, respectively. 
-#' (Defaults to \code{NULL}.) (Note: Single entry only, 
-#' use only if the same method was used for all tested compounds.)
-#'
+#' @param analysis.instrument (Character) The instrument used for chemical analysis, 
+#' for example "Waters Xevo TQ-S micro (QEB0036)". (Defaults to \code{NULL}.) 
+#' (Note: Single entry only, use only if the same instrument was used for all tested compounds.) 
+#' 
 #' @param analysis.instrument.col (Character) Column name containing \code{analysis.instrument} 
 #' information. (Defaults to "Analysis.Instrument".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{analysis.instrument}.)
 #' 
-#' @param analysis.instrument (Character) The instrument used for chemical analysis, 
-#' for example "Waters Xevo TQ-S micro (QEB0036)". (Defaults to \code{NULL}.) 
-#' (Note: Single entry only, use only if the same instrument was used for all tested compounds.) 
+#' @param analysis.parameters (Numeric) The parameters used to identify the 
+#' compound on the chemical analysis instrument. (Defaults to \code{NULL}.) 
+#' (Note: Single entry only, use only if the same parameters were used for all tested compounds.)
 #'
 #' @param analysis.parameters.col (Character) Column name containing \code{analysis.parameters} 
 #' information. (Defaults to "Analysis.Parameters".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{analysis.parameters}.)
 #' 
-#' @param analysis.parameters (Numeric) The parameters used to identify the 
-#' compound on the chemical analysis instrument. (Defaults to \code{NULL}.) 
-#' (Note: Single entry only, use only if the same parameters were used for all tested compounds.) 
-#' 
 #' @param note.col (Character) Column name of \code{data.in} containing additional notes on 
 #' test compounds. (Defaults to "Note").
-#'
+#' 
+#' @param level0.file (Character) The Level-0 file from which the \code{data.in} were obtained.
+#' (Defaults to \code{NULL}.) (Note: Single entry only, use only if all rows in data.in
+#' were obtained from the same Level-0 file.) 
+#' 
 #' @param level0.file.col (Character) Column name containing \code{level0.file} information. 
 #' (Defaults to "Level0.File".) (Note: \code{data.in} does not
 #' necessarily have this field. If this field is missing, it can be auto-filled with the value 
 #' specified in \code{level0.file}.)
-#'
-#' @param level0.file (Character) The Level-0 file from which the \code{data.in} were obtained.
-#' (Defaults to \code{NULL}.) (Note: Single entry only, use only if all rows in data.in
-#' were obtained from the same Level-0 file.) 
-#'
-#' @param level0.sheet.col (Character) Column name containing \code{level0.sheet} information.
-#' (Defaults to "Level0.Sheet".) (Note: \code{data.in} does not
-#' necessarily have this field. If this field is missing, it can be auto-filled with the value 
-#' specified in \code{level0.sheet}.)
-#'
+#' 
 #' @param level0.sheet (Character) The specific sheet name of Level-0 file from which the 
 #' \code{data.in} is obtained from, if the Level-0 file is an Excel workbook. 
 #' (Defaults to \code{NULL}.) (Note: Single entry only, use only if all rows in \code{data.in}
 #' were obtained from the same sheet in the same Level-0 file.) 
+#' 
+#' @param level0.sheet.col (Character) Column name containing \code{level0.sheet} information.
+#' (Defaults to "Level0.Sheet".) (Note: \code{data.in} does not
+#' necessarily have this field. If this field is missing, it can be auto-filled with the value 
+#' specified in \code{level0.sheet}.)
 #'
 #' @return A Level-1 data frame with a standardized format containing a  
 #' standardized set of columns and column names with hepatic
@@ -237,8 +236,8 @@ format_clint <- function(data.in,
   cal.col="Cal",
   dilution=NULL,
   dilution.col="Dilution.Factor",
-  time.col="Time",
   time = 2,
+  time.col="Time",
   istd.col="ISTD.Area",
   istd.name=NULL,
   istd.name.col="ISTD.Name",
@@ -256,10 +255,10 @@ format_clint <- function(data.in,
   analysis.parameters=NULL,
   analysis.parameters.col="Analysis.Parameters",
   note.col="Note",
-  level0.file.col="Level0.File",
   level0.file=NULL,
-  level0.sheet.col="Level0.Sheet",
-  level0.sheet=NULL
+  level0.file.col="Level0.File",
+  level0.sheet=NULL,
+  level0.sheet.col="Level0.Sheet"
   )
 {
   data.in <- as.data.frame(data.in)
