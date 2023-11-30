ee38554ade8d39d02bf7bf57f697769f6530ea63
Zhao <Zhao.Zhihui@epa.gov>
2023-11-30 10:09:48 -0500

    Added a comment reminding to load the old version of data when want to re-produce the steps.

--- invitroTKstats/data-raw/clean_up_wambaugh2019.R
+++ invitroTKstats/data-raw/clean_up_wambaugh2019.R
@@ -5,6 +5,10 @@
 # The previous version of dataset is saved under data-raw as wambaugh2019_old.RData
 # load("~/invitrotkstats/invitroTKstats/Data/wambaugh2019.RData")
 
+# If there is a need to re-produce the steps, you will need to run the following code instead 
+# to load the previous version of wambaugh2019 dataset.
+# load("~/invitrotkstats/invitroTKstats/Data/wambaugh2019_old.RData")
+
 library(dplyr)
 # remove irrelevant variables and save the updated datasets
 wambaugh2019.clint <- wambaugh2019.clint %>% select(-X, -Transition, -`ln...Remaining`, -TaskOrder)
