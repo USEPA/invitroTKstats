5758431a9fdb41eb2737fabe5c0ed805d599efc6
Zhao <Zhao.Zhihui@epa.gov>
2024-02-22 11:51:25 -0500

    Updated the documented example, removed steps before level-2

--- invitroTKstats/R/calc_caco2_point.R
+++ invitroTKstats/R/calc_caco2_point.R
@@ -68,41 +68,27 @@
 #' @author John Wambaugh
 #'
 #' @examples
-#' library(invitroTKstats)
+#' ## Load example level-2 data
+#' level2 <- invitroTKstats::caco2.level2
+#' 
+#' ## scenario 1: 
+#' ## input level-2 data from the R session and do not export the result table
+#' level3 <- calc_caco2_point(data.in = level2, output.res = FALSE)
 #'
-#' level0 <- TO1caco2
-#' level1 <- format_caco2(level0,
-#'    FILENAME="EPACyprotex2021",
-#'    sample.col="SampleName",
-#'    dtxsid.col="CompoundName",
-#'    lab.compound.col="CompoundName",
-#'    cal=1,
-#'    istd.conc.col="ISTD.Conc",
-#'    compound.col="CompoundName",
-#'    compound.conc.col="Test.Target.Conc",
-#'    membrane.area=0.11,
-#'    series=1,
-#'    analysis.parameters="Feature",
-#'    analysis.instrument="GC or LC",
-#'    analysis.method="Mass Spec"
-#'    )
-#'
-#' level2 <- level1
-#' level2$Verified <- "Y"
-#'
+#' ## scenario 2: 
+#' ## import level-2 data from a 'tsv' file and export the result table
+#' 
 #' write.table(level2,
-#'   file="EPACyprotex2021-Caco-2-Level2.tsv",
+#'   file="invitroTKstats/data-raw/EPACyprotex2021-Caco-2-Level2.tsv",
 #'   sep="\t",
 #'   row.names=F,
 #'   quote=F)
+#' 
+#' ## Unless a different path is specified in OUTPUT.DIR,
+#' ## the result table will be saved to the directory specified in INPUT.DIR
+#' level3 <- calc_caco2_point(FILENAME="EPACyprotex2021", 
+#' INPUT.DIR = "invitroTKstats/data-raw")
 #'
-#' level3 <- calc_caco2_point(FILENAME="EPACyprotex2021")
-#'
-#' write.table(level3,
-#'   file="EPACyprotex2021-Caco-2-Level3.tsv",
-#'   sep="\t",
-#'   row.names=F,
-#'   quote=F)
 #'
 #' @references
 #' \insertRef{hubatsch2007determination}{invitroTKstats}
