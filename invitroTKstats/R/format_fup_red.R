413b1bac7aff17f4dc3ec72d799ed19e620b6c02
Zhao <Zhao.Zhihui@epa.gov>
2024-04-03 12:02:14 -0400

    Updated the comments in the documented example for format_fup_red

--- invitroTKstats/R/format_fup_red.R
+++ invitroTKstats/R/format_fup_red.R
@@ -231,33 +231,31 @@
 #' level0 <- invitroTKstats::fup_red_L0
 #' 
 #' ## Run it through level-1 processing function
-#' ## This example shows the use of data.in argument which allows users to pass
+#' ## This example shows the use of the data.in argument which allows users to pass
 #' ## in a data frame from the R session.
 #' ## If the input level-0 data exists in an external file such as a .tsv file,
-#' ## users may import it using INPUT.DIR to specify the path and FILENAME
-#' ## to specify the file name. See documentation for details.
+#' ## users may import it using FILENAME and INPUT.DIR to specify the file name 
+#' ## and its directory path, respectively.
 #' fup_red_L1 <- format_fup_red(data.in = fup_red_L0,
-#'                              sample.col ="Name",
-#'                              date.col="Acq.Date",
+#'                              sample.col ="Sample",
+#'                              date.col="Date",
 #'                              compound.col="Compound",
 #'                              lab.compound.col="Compound",
 #'                              type.col="Sample.Type",
 #'                              dilution.col="Dilution.Factor",
 #'                              technical.replicates.col ="Replicate",
 #'                              cal=1,
+#'                              area.col = "Peak.Area",
 #'                              istd.conc = 10/1000,
-#'                              istd.col= "IS Area",
-#'                              istd.name.col = "ISTD", 
-#'                              test.conc.col = "Std. Conc", 
-#'                              level0.file.col = "File", 
-#'                              level0.sheet.col = "Sheet",
+#'                              istd.col= "ISTD.Peak.Area",
+#'                              test.conc.col = "Compound.Conc", 
 #'                              test.nominal.conc = 10,
 #'                              plasma.percent = 100,
 #'                              time.col = "Time",
 #'                              analysis.method = "LCMS",
 #'                              analysis.instrument = "Waters ACQUITY I-Class UHPLC - Xevo TQ-S uTQMS",
 #'                              analysis.parameters = "RT",
-#'                              note.col=NULL,
+#'                              note.col=NULL, 
 #'                              output.res = FALSE
 #'                              )
 #' 
